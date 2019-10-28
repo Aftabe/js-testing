e15158b152700109e0180d892665a2c053f9facc
Ali Khan <shamsasghari77@gmail.com>
2019-10-28 17:42:11 +1100

    input form and button added

--- js/style.js
+++ js/style.js
@@ -1,5 +1,11 @@
 function doSomething() {
    var string = document.getElementById('demo').innerHTML;
    document.getElementById('demo').innerHTML = string.toLowerCase();
+}
 
+function myFunction() {
+   var age, voteable;
+   age = document.getElementById('age').value;
+   voteable = (age < 18) ? "Too young" : "Old enough";
+   document.getElementById("demo1").innerHTML = voteable + " to vote.";
 }
