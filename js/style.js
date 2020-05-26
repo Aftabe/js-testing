36bd87fcf73ca6926a8151d2d6688efd31007de2
Sunny <47844914+Aftabe@users.noreply.github.com>
2020-05-26 16:56:10 +1000

    js code added

---
+++ js/style.js
@@ -0,0 +1,18 @@
+// testing
+var cars = ["", "Volvo", "BMW"];
+
+
+cars[0] = new Date();
+
+
+// mod;
+
+
+// mod = cars[0].push('Toyota');
+
+
+
+
+
+document.getElementById("demo").innerHTML = cars;
+console.log(typeof mod);
