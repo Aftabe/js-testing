3e08cf40c6e0a73dadf1f97e0abfd1d638d6c3a8
Sunny <47844914+Aftabe@users.noreply.github.com>
2020-05-28 19:19:42 +1000

    new statements added to the js file

--- js/style.js
+++ js/style.js
@@ -11,7 +11,7 @@ cars[2] = myFunction('Ali', 'Karimi');
 
 // mod = cars[0].push('Toyota');
 
-function myFunction(p1, p2){
+function myFunction(p1, p2) {
    var txt = 'Hey dear ' + p1 + " " + p2 + ' Welcome to our party';
    // document.getElementById('demoA').innerHTML = txt;
    return txt;
@@ -19,8 +19,8 @@ function myFunction(p1, p2){
 
 
 
-document.getElementById("demo").innerHTML = cars;
-console.log(typeof mod);
+// document.getElementById("demo").innerHTML = cars;
+// console.log(typeof mod);
 
 
 // new arrays
@@ -31,10 +31,19 @@ var numbers = [20, 40, 10, 60, 4];
 persons.sort();
 delete numbers[1];
 
-numbers.sort(function(a, b){
+numbers.sort(function (a, b) {
    return a - b;
 });
-
-
 document.getElementById('js-demo').innerHTML = persons;
 document.getElementById('js-demo').innerHTML = numbers;
+
+
+(function () {
+   var fruits = ["Banana", "Orange", "Apple", "Mango"];
+   document.getElementById("demo1").innerHTML = "Original Array:<br>" + fruits;
+   function myFunction(){
+      fruits.splice(2, 0, "Lemon", "Kiwi");
+      document.getElementById("demo2").innerHTML = "New Array:<br>" + fruits;
+   }
+   myFunction();
+})();
