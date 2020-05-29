4441c4d01d531a69c286272f5b72ae16947a4dd5
Sunny <47844914+Aftabe@users.noreply.github.com>
2020-05-29 18:23:38 +1000

    code modifed

--- js/style.js
+++ js/style.js
@@ -1,52 +1,51 @@
-// testing
-var cars = ["", "Volvo", "BMW"];
+// // testing
+// var cars = ["", "Volvo", "BMW"];
 
 
-cars[0] = new Date();
-cars[2] = myFunction('Ali', 'Karimi');
+// cars[0] = new Date();
+// cars[2] = myFunction('Ali', 'Karimi');
 
 
-// mod;
+// // mod;
 
 
-// mod = cars[0].push('Toyota');
+// // mod = cars[0].push('Toyota');
 
-function myFunction(p1, p2) {
-   var txt = 'Hey dear ' + p1 + " " + p2 + ' Welcome to our party';
-   // document.getElementById('demoA').innerHTML = txt;
-   return txt;
+// function myFunction(p1, p2) {
+//    var txt = 'Hey dear ' + p1 + " " + p2 + ' Welcome to our party';
+//    // document.getElementById('demoA').innerHTML = txt;
+//    return txt;
+// }
+
+
+
+// // document.getElementById("demo").innerHTML = cars;
+// // console.log(typeof mod);
+
+
+// // new arrays
+// var persons = ['Ali', 'Khadim', 'Sadiq', 'Bashir'];
+// var numbers = [20, 40, 10, 60, 4];
+
+
+// persons.sort();
+// delete numbers[1];
+
+// numbers.sort(function (a, b) {
+//    return a - b;
+// });
+// document.getElementById('js-demo').innerHTML = persons;
+// document.getElementById('js-demo').innerHTML = numbers;
+
+
+// (function () {
+// })();
+
+
+
+var fruits = ["Banana", "Orange", "Apple", "Mango"];
+document.getElementById("demo1").innerHTML = "Original Array:<br>" + fruits;
+function myFunction() {
+   fruits.splice(2, 0, "Lemon", "Kiwi");
+   document.getElementById("demo2").innerHTML = "New Array:<br>" + fruits;
 }
-
-
-
-// document.getElementById("demo").innerHTML = cars;
-// console.log(typeof mod);
-
-
-// new arrays
-var persons = ['Ali', 'Khadim', 'Sadiq', 'Bashir'];
-var numbers = [20, 40, 10, 60, 4];
-
-
-persons.sort();
-delete numbers[1];
-
-numbers.sort(function (a, b) {
-   return a - b;
-});
-document.getElementById('js-demo').innerHTML = persons;
-document.getElementById('js-demo').innerHTML = numbers;
-
-
-(function() {
-   var fruits = ["Banana", "Orange", "Apple", "Mango"];
-   document.getElementById("demo1").innerHTML = "Original Array:<br>" + fruits;
-   function myFunction(){
-      fruits.splice(2, 0, "Lemon", "Kiwi");
-      document.getElementById("demo2").innerHTML = "New Array:<br>" + fruits;
-   }
-
-   var call = myFunction();
-   alert(call);
-   // myFunction();
-})();
