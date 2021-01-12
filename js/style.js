447a7d760d3f1fdeecd27eefc6c3186f9d0f6b19
Aftabe <shamsasghari77@gmail.com>
2021-01-12 18:36:32 +1100

    New js statments are created

--- js/style.js
+++ js/style.js
@@ -1,9 +1,9 @@
 const btns = document.querySelectorAll('button');
 for(i = 0; i < btns.length; i++){
    btns[i].onclick = function(){
-      // let classL = this.innerHTML.toLowerCase();
-      // document.body.style.backgroundColor = 'green';
-      document.body.classList.add('grey');
+      var className = this.innerHTML.toLowerCase();
+      document.body.className = className;
+
     
 
       
