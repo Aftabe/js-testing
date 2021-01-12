6ede25ac70ea9eea26c85bee48a389b2dcbb2f30
Aftabe <shamsasghari77@gmail.com>
2021-01-12 18:21:15 +1100

    some js statements are modified

--- js/style.js
+++ js/style.js
@@ -2,6 +2,10 @@ const btns = document.querySelectorAll('button');
 for(i = 0; i < btns.length; i++){
    btns[i].onclick = function(){
       // let classL = this.innerHTML.toLowerCase();
-      document.body.style.backgroundColor = 'green';
+      // document.body.style.backgroundColor = 'green';
+      document.body.classList.add('grey');
+    
+
+      
    }
 }
