8ba10904045c7230634ad564af80725a732f6855
Aftabe <shamsasghari77@gmail.com>
2021-01-18 21:00:39 +1100

    some new js statements added

--- js/style.js
+++ js/style.js
@@ -1,8 +1,15 @@
-const btns = document.querySelectorAll('button');
-for(i = 0; i < btns.length; i++){
-   btns[i].onclick = function(){
-      var className = this.innerHTML.toLowerCase();
-      document.body.className = className;
-      // document.body.className = 'border';
+(function () {
+   const btns = document.querySelectorAll('button');
+   for (i = 0; i < btns.length; i++) {
+      btns[i].onclick = function () {
+         var className = this.innerHTML.toLowerCase();
+         document.body.className = className;
+         // document.body.className = 'border';
+      }
    }
-}
+}())
+
+// This is for tesing js
+document.addEventListener('click', function(){
+   document.body.style.backgroundColor = 'green';
+})
